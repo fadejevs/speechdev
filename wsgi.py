c9e836d9dc9771e09ba2bc61aa98ac95a1f0a71d
fadejevs <ralphfdesigns@gmail.com>
2025-04-07 12:12:34 +0300

    Removed allow_unsafe_werkzeug parameter

--- wsgi.py
+++ wsgi.py
@@ -4,8 +4,7 @@ from app import app, socketio
 if __name__ == "__main__":
     # Get port from environment variable or use 5000 as default
     port = int(os.environ.get("PORT", 5000))
-    # Use socketio.run with gevent
+    # Use socketio.run without the allow_unsafe_werkzeug parameter
     socketio.run(app, 
                 host='0.0.0.0', 
-                port=port,
-                allow_unsafe_werkzeug=True)
+                port=port)
