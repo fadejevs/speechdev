31dd80491bb3c532a5ad9a607b9583855a02ed91
fadejevs <ralphfdesigns@gmail.com>
2025-04-07 11:58:31 +0300

    added render yaml start file

--- wsgi.py
+++ wsgi.py
@@ -4,5 +4,8 @@ from app import app, socketio
 if __name__ == "__main__":
     # Get port from environment variable or use 5000 as default
     port = int(os.environ.get("PORT", 5000))
-    # Use socketio.run instead of app.run for WebSocket support
-    socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
+    # Use socketio.run with gevent
+    socketio.run(app, 
+                host='0.0.0.0', 
+                port=port,
+                allow_unsafe_werkzeug=True)
